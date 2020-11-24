eb812ed6b00056b4c066c676b1877e88ff6033eb
Marcio Valle De Oliveira e Cruz <mvocruz@hotmail.com>
2020-11-24 20:36:20 +0000

    create assistance request tables

--- migrations/assignments_submissions.sql
+++ migrations/assignments_submissions.sql
@@ -7,7 +7,7 @@ CREATE TABLE assignments (
   duration INTEGER
 );
 
-CREATE TABLE assignment_submissions (
+CREATE TABLE assignment_submission (
   id SERIAL PRIMARY KEY NOT NULL,
   assignment_id INTEGER REFERENCES assignments(id) ON DELETE CASCADE,
   student_id INTEGER REFERENCES students(id) ON DELETE CASCADE,
