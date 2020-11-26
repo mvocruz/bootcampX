72bf6be034f2644c277a4e6c1006c97348bc94ac
Marcio Valle De Oliveira e Cruz <mvocruz@hotmail.com>
2020-11-26 04:33:13 +0000

    create queries with pg module

---
+++ bootcamp_app/students.js
@@ -0,0 +1,29 @@
+const pg = require('pg');
+const pool = pg.Pool
+
+
+const config = {
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+};
+
+pool.connect(() => {
+  console.log('connected to the database');
+});
+
+let cohort = process.argv[2];
+
+pool.query(`
+SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+FROM students
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohort.name = ${cohort}
+LIMIT ${process.argv[3] || 5};
+`)
+.then(res => {
+  res.rows.forEach(user => {
+    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+})
+}).catch(err => console.error('query error', err.stack));
---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,33 @@
+const pg = require('pg');
+const pool = pg.Pool
+
+
+const config = {
+  user: 'vagrant',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+};
+
+pool.connect(() => {
+  console.log('connected to the database');
+});
+
+let cohort = process.argv[2];
+
+pool.query(`
+SELECT DISTINCT teachers.name AS teacher, 
+                cohorts.name AS cohort
+FROM teachers
+JOIN assistance_requests ON teachers.id = teacher_id
+JOIN students ON students.id = assistance_requests.student_id
+JOIN cohorts ON cohorts.id = cohort_id
+WHERE cohorts.name = '${cohort  || 'JUL02'}'
+ORDER BY teacher;
+`)
+.then(res => {
+  res.rows.forEach(row => {
+    console.log(`${row.cohort}: ${row.teacher}`);
+  })
+})
+.catch(err => console.error('query error', err.stack));
