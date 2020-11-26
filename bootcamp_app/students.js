ad4b7b21b99fd5930883e3c63bee58ebde43c854
Marcio Valle De Oliveira e Cruz <mvocruz@hotmail.com>
2020-11-26 08:33:30 +0000

    protect from code injection

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -13,15 +13,16 @@ pool.connect(() => {
   console.log('connected to the database');
 });
 
-let cohort = process.argv[2];
+let cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
 
 pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohort.name = ${cohort}
-LIMIT ${process.argv[3] || 5};
-`)
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohort.name = $1
+  LIMIT $2;
+  `, [`%${cohortName}%`, limit])
 .then(res => {
   res.rows.forEach(user => {
     console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -16,15 +16,15 @@ pool.connect(() => {
 let cohort = process.argv[2];
 
 pool.query(`
-SELECT DISTINCT teachers.name AS teacher, 
+  SELECT DISTINCT teachers.name AS teacher, 
                 cohorts.name AS cohort
-FROM teachers
-JOIN assistance_requests ON teachers.id = teacher_id
-JOIN students ON students.id = assistance_requests.student_id
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name = '${cohort  || 'JUL02'}'
-ORDER BY teacher;
-`)
+  FROM teachers
+  JOIN assistance_requests ON teachers.id = teacher_id
+  JOIN students ON students.id = assistance_requests.student_id
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name = 
+  ORDER BY teacher; $1
+  `, [`${cohort  || 'JUL02'}`])
 .then(res => {
   res.rows.forEach(row => {
     console.log(`${row.cohort}: ${row.teacher}`);
